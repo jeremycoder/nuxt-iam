6dadd53b40e1a749d37322874737aa3f13571698
Jeremy Mwangelwa <jeremy@nizamedia.com>
2023-01-15 12:40:03 -0500

    Add dashboard

--- components/iamDashboard.vue
+++ components/iamDashboard.vue
@@ -1,141 +1,102 @@
 <template>
-  <header>
-    <nav class="navbar navbar-default">
-      <div class="navbar-header">
-        <a class="navbar-brand" href="#"
-          ><i class="fa fa-cube"></i
-          ><span style="color: #184b81">Nuxt<b>IAM</b></span></a
-        >
-        <button
-          type="button"
-          data-target="#navbarCollapse"
-          data-toggle="collapse"
-          class="navbar-toggle"
-        >
-          <span class="navbar-toggler-icon"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-      </div>
-      <!-- Collection of nav links, forms, and other content for toggling -->
-      <div id="navbarCollapse" class="collapse navbar-collapse">
-        <ul class="nav navbar-nav">
-          <li class="active"><a href="#">Home</a></li>
-          <li><a href="#">Admin</a></li>
-        </ul>
-
-        <ul class="nav navbar-nav navbar-right">
-          <li :class="showProfileNav ? 'dropdown open' : ''">
-            <!-- add open to toggle open -->
-            <a
-              href="#"
-              data-toggle="dropdown"
-              class="dropdown-toggle user-action"
-              :aria-expanded="showProfileNav ? true : false"
-              @click="toggleShowProfileNav"
+  <div v-if="isLoaded">
+    <div v-if="isLoggedIn">
+      <!-- Check if email is verified -->
+      <div
+        v-if="verifyRegistrations && !emailIsVerified"
+        class="container-xl px-4 mt-4"
+      >
+        <div>
+          <h2>Email verification is required</h2>
+          <h4 v-if="!verificationEmailSent">
+            <p>Please click the button below to verify your email</p>
+            <button
+              class="btn btn-primary"
+              type="button"
+              @click="verifyMyEmail(profile.email)"
             >
-              <!-- change to true to open menu, then false to close menu
-             -->
-              <img
-                src="https://www.tutorialrepublic.com/examples/images/avatar/2.jpg"
-                class="avatar"
-                alt="Avatar" />
-              {{ profile.firstName }} {{ profile.lastName }}
-              <b class="caret"></b
-            ></a>
-            <ul class="dropdown-menu">
-              <li>
-                <a href="#"><i class="fa fa-user-o"></i> Profile</a>
-              </li>
-              <li>
-                <a href="#"><i class="fa fa-sliders"></i> Settings</a>
-              </li>
-              <li class="divider"></li>
-              <li>
-                <a href="#" @click="logMeOut">Logout</a>
-              </li>
-            </ul>
-          </li>
-        </ul>
+              Send email verification
+            </button>
+          </h4>
+          <h4 v-else>
+            <p>
+              Please check your email. Check your spam folder too. You should
+              receive it within 15 minutes.
+            </p>
+          </h4>
+        </div>
       </div>
-    </nav>
-  </header>
+      <!-- Main content -->
+      <div v-else>
+        <header>
+          <nav class="navbar navbar-default">
+            <div class="navbar-header">
+              <a class="navbar-brand" href="#"
+                ><i class="fa fa-cube"></i
+                ><span style="color: #184b81">Nuxt<b>IAM</b></span></a
+              >
+              <button
+                type="button"
+                data-target="#navbarCollapse"
+                data-toggle="collapse"
+                class="navbar-toggle"
+                @click="toggleShowMobileNav"
+              >
+                <span class="navbar-toggler-icon"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+            </div>
+            <div
+              id="navbarCollapse"
+              class="navbar-collapse"
+              :class="showMobileNav ? '' : 'collapse'"
+            >
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Admin</a></li>
+              </ul>
 
-  <div class="container">
-    <div class="table-wrapper">
-      <table class="table table-striped">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Name</th>
-            <th>Address</th>
-            <th>City</th>
-            <th>Pin Code</th>
-            <th>Country</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Thomas Hardy</td>
-            <td>89 Chiaroscuro Rd.</td>
-            <td>Portland</td>
-            <td>97219</td>
-            <td>USA</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Maria Anders</td>
-            <td>Obere Str. 57</td>
-            <td>Berlin</td>
-            <td>12209</td>
-            <td>Germany</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>Fran Wilson</td>
-            <td>C/ Araquil, 67</td>
-            <td>Madrid</td>
-            <td>28023</td>
-            <td>Spain</td>
-          </tr>
-          <tr>
-            <td>4</td>
-            <td>Dominique Perrier</td>
-            <td>25, rue Lauriston</td>
-            <td>Paris</td>
-            <td>75016</td>
-            <td>France</td>
-          </tr>
-          <tr>
-            <td>5</td>
-            <td>Martin Blank</td>
-            <td>Via Monte Bianco 34</td>
-            <td>Turin</td>
-            <td>10100</td>
-            <td>Italy</td>
-          </tr>
-        </tbody>
-      </table>
-      <div class="text-center">
-        <ul class="pagination justify-content-center">
-          <li class="page-item disabled">
-            <a href="#"><i class="fa fa-long-arrow-left"></i> Previous</a>
-          </li>
-          <li class="page-item"><a href="#" class="page-link">1</a></li>
-          <li class="page-item"><a href="#" class="page-link">2</a></li>
-          <li class="page-item active"><a href="#" class="page-link">3</a></li>
-          <li class="page-item"><a href="#" class="page-link">4</a></li>
-          <li class="page-item"><a href="#" class="page-link">5</a></li>
-          <li class="page-item">
-            <a href="#" class="page-link"
-              >Next <i class="fa fa-long-arrow-right"></i
-            ></a>
-          </li>
-        </ul>
+              <ul class="nav navbar-nav navbar-right">
+                <li :class="showProfileNav ? 'dropdown open' : ''">
+                  <a
+                    href="#"
+                    data-toggle="dropdown"
+                    class="dropdown-toggle user-action"
+                    @click="toggleShowProfileNav"
+                  >
+                    <img
+                      src="https://www.tutorialrepublic.com/examples/images/avatar/2.jpg"
+                      class="avatar"
+                      alt="Avatar" />
+                    {{ profile.firstName }} {{ profile.lastName }}
+                    <b class="caret"></b
+                  ></a>
+                  <ul class="dropdown-menu">
+                    <li>
+                      <a href="#"><i class="fa fa-user-o"></i> Profile</a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fa fa-sliders"></i> Settings</a>
+                    </li>
+                    <li class="divider"></li>
+                    <li>
+                      <a href="#" @click="logMeOut">Logout</a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </header>
+
+        <div class="container"></div>
       </div>
     </div>
+  </div>
+  <div v-else class="container-xl px-4 mt-4">
+    <div class="spinner-border" role="status"></div>
   </div>
 </template>
 
@@ -177,7 +138,6 @@ function toggleShowMobileNav() {
  */
 function toggleShowProfileNav() {
   showProfileNav.value = !showProfileNav.value;
-  console.log("show profile nav: ", showProfileNav.value);
 }
 
 // User profile
@@ -200,7 +160,6 @@ onMounted(async () => {
 
 async function isLoggedIn() {
   iAmLoggedIn.value = await isAuthenticated();
-  console.log("isLoggedIn: ", iAmLoggedIn.value);
 
   // If user is not authenticated, push to login page
   if (!iAmLoggedIn.value) router.push("/iam/login");
@@ -209,7 +168,6 @@ async function isLoggedIn() {
 // Log user out
 async function logMeOut() {
   const { status } = await logout();
-  console.log("status: ", status);
   if (status === "success") {
     router.push("/iam/login");
   }
@@ -227,8 +185,6 @@ async function getMyProfile() {
 
   // If successful, data will contain profile
   if (status === "success") {
-    console.log("status: ", status);
-    console.log("data: ", data);
     profile.uuid = data.uuid;
     profile.firstName = data.first_name;
     profile.lastName = data.last_name;
@@ -243,7 +199,6 @@ async function getMyProfile() {
 
 // Attempt to update user profile
 async function updateMyProfile(profile) {
-  console.log("profile: ", profile);
   const { error } = await updateProfile(
     profile.uuid,
     profile.firstName,
@@ -302,8 +257,6 @@ async function updateMyProfileWithPassword(profile) {
     return;
   }
 
-  console.log("status: ", status);
-  console.log("data: ", data);
   updateSuccessful.value = true;
 }
 
@@ -324,7 +277,6 @@ async function deleteMyAccount(profile) {
 
 // Verify my email
 async function verifyMyEmail(email) {
-  console.log("Verifying my email: ", email);
   verifyEmail(email);
   verificationEmailSent.value = true;
 }
@@ -350,12 +302,6 @@ useHead({
     rel: "stylesheet",
     href: "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css",
     type: "text/css",
-  },
-  script: {
-    src: "https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js",
-  },
-  script: {
-    src: "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js",
   },
 });
 </script>
