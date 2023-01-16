a42b47355514f13b4690bcbe22f8937dbf46abc0
Jeremy Mwangelwa <jeremy@nizamedia.com>
2023-01-16 12:25:23 -0500

    Can update user profile!

--- pages/iam/dashboard/admin.vue
+++ pages/iam/dashboard/admin.vue
@@ -3,8 +3,390 @@
     <div class="page-header">
       <h2>Admin</h2>
     </div>
-    <p>User administration data goes here</p>
+    <p>Administer users here.</p>
+    <!-- Users table -->
+    <div>
+      <div class="table-title">
+        <div class="row">
+          <div class="col-sm-8">
+            <h2>Users</h2>
+          </div>
+          <div class="col-sm-4">
+            <button type="button" class="btn btn-info add-new">
+              <i class="fa fa-plus"></i> Add New
+            </button>
+          </div>
+        </div>
+      </div>
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Department</th>
+            <th>Phone</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>John Doe</td>
+            <td>Administration</td>
+            <td>(171) 555-2222</td>
+            <td>
+              <a class="add" title="Add" data-toggle="tooltip"
+                ><i class="material-icons">&#xE03B;</i></a
+              >
+              <a class="edit" title="Edit" data-toggle="tooltip"
+                ><i class="material-icons">&#xE254;</i></a
+              >
+              <a class="delete" title="Delete" data-toggle="tooltip"
+                ><i class="material-icons">&#xE872;</i></a
+              >
+            </td>
+          </tr>
+          <tr>
+            <td>Peter Parker</td>
+            <td>Customer Service</td>
+            <td>(313) 555-5735</td>
+            <td>
+              <a class="add" title="Add" data-toggle="tooltip"
+                ><i class="material-icons">&#xE03B;</i></a
+              >
+              <a class="edit" title="Edit" data-toggle="tooltip"
+                ><i class="material-icons">&#xE254;</i></a
+              >
+              <a class="delete" title="Delete" data-toggle="tooltip"
+                ><i class="material-icons">&#xE872;</i></a
+              >
+            </td>
+          </tr>
+          <tr>
+            <td>Fran Wilson</td>
+            <td>Human Resources</td>
+            <td>(503) 555-9931</td>
+            <td>
+              <a class="add" title="Add" data-toggle="tooltip"
+                ><i class="material-icons">&#xE03B;</i></a
+              >
+              <a class="edit" title="Edit" data-toggle="tooltip"
+                ><i class="material-icons">&#xE254;</i></a
+              >
+              <a class="delete" title="Delete" data-toggle="tooltip"
+                ><i class="material-icons">&#xE872;</i></a
+              >
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <!-- Users table permissions-->
+    <div>
+      <div class="table-title">
+        <div class="row">
+          <div class="col-sm-8">
+            <h2>Users Table <b>Permissions</b></h2>
+          </div>
+          <div class="col-sm-4">
+            <button type="button" class="btn btn-info add-new">
+              <i class="fa fa-plus"></i> Add New
+            </button>
+          </div>
+        </div>
+      </div>
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Department</th>
+            <th>Phone</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>John Doe</td>
+            <td>Administration</td>
+            <td>(171) 555-2222</td>
+            <td>
+              <a class="add" title="Add" data-toggle="tooltip"
+                ><i class="material-icons">&#xE03B;</i></a
+              >
+              <a class="edit" title="Edit" data-toggle="tooltip"
+                ><i class="material-icons">&#xE254;</i></a
+              >
+              <a class="delete" title="Delete" data-toggle="tooltip"
+                ><i class="material-icons">&#xE872;</i></a
+              >
+            </td>
+          </tr>
+          <tr>
+            <td>Peter Parker</td>
+            <td>Customer Service</td>
+            <td>(313) 555-5735</td>
+            <td>
+              <a class="add" title="Add" data-toggle="tooltip"
+                ><i class="material-icons">&#xE03B;</i></a
+              >
+              <a class="edit" title="Edit" data-toggle="tooltip"
+                ><i class="material-icons">&#xE254;</i></a
+              >
+              <a class="delete" title="Delete" data-toggle="tooltip"
+                ><i class="material-icons">&#xE872;</i></a
+              >
+            </td>
+          </tr>
+          <tr>
+            <td>Fran Wilson</td>
+            <td>Human Resources</td>
+            <td>(503) 555-9931</td>
+            <td>
+              <a class="add" title="Add" data-toggle="tooltip"
+                ><i class="material-icons">&#xE03B;</i></a
+              >
+              <a class="edit" title="Edit" data-toggle="tooltip"
+                ><i class="material-icons">&#xE254;</i></a
+              >
+              <a class="delete" title="Delete" data-toggle="tooltip"
+                ><i class="material-icons">&#xE872;</i></a
+              >
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <!-- Refresh tokens -->
+    <div>
+      <div class="table-title">
+        <div class="row">
+          <div class="col-sm-8">
+            <h2>Refresh <b>Tokens</b></h2>
+          </div>
+          <div class="col-sm-4">
+            <button type="button" class="btn btn-info add-new">
+              <i class="fa fa-plus"></i> Add New
+            </button>
+          </div>
+        </div>
+      </div>
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Department</th>
+            <th>Phone</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>John Doe</td>
+            <td>Administration</td>
+            <td>(171) 555-2222</td>
+            <td>
+              <a class="add" title="Add" data-toggle="tooltip"
+                ><i class="material-icons">&#xE03B;</i></a
+              >
+              <a class="edit" title="Edit" data-toggle="tooltip"
+                ><i class="material-icons">&#xE254;</i></a
+              >
+              <a class="delete" title="Delete" data-toggle="tooltip"
+                ><i class="material-icons">&#xE872;</i></a
+              >
+            </td>
+          </tr>
+          <tr>
+            <td>Peter Parker</td>
+            <td>Customer Service</td>
+            <td>(313) 555-5735</td>
+            <td>
+              <a class="add" title="Add" data-toggle="tooltip"
+                ><i class="material-icons">&#xE03B;</i></a
+              >
+              <a class="edit" title="Edit" data-toggle="tooltip"
+                ><i class="material-icons">&#xE254;</i></a
+              >
+              <a class="delete" title="Delete" data-toggle="tooltip"
+                ><i class="material-icons">&#xE872;</i></a
+              >
+            </td>
+          </tr>
+          <tr>
+            <td>Fran Wilson</td>
+            <td>Human Resources</td>
+            <td>(503) 555-9931</td>
+            <td>
+              <a class="add" title="Add" data-toggle="tooltip"
+                ><i class="material-icons">&#xE03B;</i></a
+              >
+              <a class="edit" title="Edit" data-toggle="tooltip"
+                ><i class="material-icons">&#xE254;</i></a
+              >
+              <a class="delete" title="Delete" data-toggle="tooltip"
+                ><i class="material-icons">&#xE872;</i></a
+              >
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <!-- Refresh tokens -->
+    <div>
+      <div class="table-title">
+        <div class="row">
+          <div class="col-sm-8">
+            <h2>One-time <b>Tokens</b></h2>
+          </div>
+          <div class="col-sm-4">
+            <button type="button" class="btn btn-info add-new">
+              <i class="fa fa-plus"></i> Add New
+            </button>
+          </div>
+        </div>
+      </div>
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Department</th>
+            <th>Phone</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>John Doe</td>
+            <td>Administration</td>
+            <td>(171) 555-2222</td>
+            <td>
+              <a class="add" title="Add" data-toggle="tooltip"
+                ><i class="material-icons">&#xE03B;</i></a
+              >
+              <a class="edit" title="Edit" data-toggle="tooltip"
+                ><i class="material-icons">&#xE254;</i></a
+              >
+              <a class="delete" title="Delete" data-toggle="tooltip"
+                ><i class="material-icons">&#xE872;</i></a
+              >
+            </td>
+          </tr>
+          <tr>
+            <td>Peter Parker</td>
+            <td>Customer Service</td>
+            <td>(313) 555-5735</td>
+            <td>
+              <a class="add" title="Add" data-toggle="tooltip"
+                ><i class="material-icons">&#xE03B;</i></a
+              >
+              <a class="edit" title="Edit" data-toggle="tooltip"
+                ><i class="material-icons">&#xE254;</i></a
+              >
+              <a class="delete" title="Delete" data-toggle="tooltip"
+                ><i class="material-icons">&#xE872;</i></a
+              >
+            </td>
+          </tr>
+          <tr>
+            <td>Fran Wilson</td>
+            <td>Human Resources</td>
+            <td>(503) 555-9931</td>
+            <td>
+              <a class="add" title="Add" data-toggle="tooltip"
+                ><i class="material-icons">&#xE03B;</i></a
+              >
+              <a class="edit" title="Edit" data-toggle="tooltip"
+                ><i class="material-icons">&#xE254;</i></a
+              >
+              <a class="delete" title="Delete" data-toggle="tooltip"
+                ><i class="material-icons">&#xE872;</i></a
+              >
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <script setup></script>
+
+<style scoped>
+.table-wrapper {
+  width: 700px;
+  margin: 30px auto;
+  background: #fff;
+  padding: 20px;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.05);
+}
+.table-title {
+  padding-bottom: 10px;
+  margin: 0 0 10px;
+}
+.table-title h2 {
+  margin: 6px 0 0;
+  font-size: 22px;
+}
+.table-title .add-new {
+  float: right;
+  height: 30px;
+  font-weight: bold;
+  font-size: 12px;
+  text-shadow: none;
+  min-width: 100px;
+  border-radius: 50px;
+  line-height: 13px;
+}
+.table-title .add-new i {
+  margin-right: 4px;
+}
+table.table {
+  table-layout: fixed;
+}
+table.table tr th,
+table.table tr td {
+  border-color: #e9e9e9;
+}
+table.table th i {
+  font-size: 13px;
+  margin: 0 5px;
+  cursor: pointer;
+}
+table.table th:last-child {
+  width: 100px;
+}
+table.table td a {
+  cursor: pointer;
+  display: inline-block;
+  margin: 0 5px;
+  min-width: 24px;
+}
+table.table td a.add {
+  color: #27c46b;
+}
+table.table td a.edit {
+  color: #ffc107;
+}
+table.table td a.delete {
+  color: #e34724;
+}
+table.table td i {
+  font-size: 19px;
+}
+table.table td a.add i {
+  font-size: 24px;
+  margin-right: -1px;
+  position: relative;
+  top: 3px;
+}
+table.table .form-control {
+  height: 32px;
+  line-height: 32px;
+  box-shadow: none;
+  border-radius: 2px;
+}
+table.table .form-control.error {
+  border-color: #f50000;
+}
+table.table td .add {
+  display: none;
+}
+</style>
--- pages/iam/dashboard/profile.vue
+++ pages/iam/dashboard/profile.vue
@@ -4,7 +4,116 @@
       <h2>Profile</h2>
     </div>
     <p>Your profile goes here</p>
+    <!-- Profile errors notification -->
+    <div
+      v-if="profileError"
+      class="alert alert-danger alert-dismissable"
+      role="alert"
+    >
+      <button
+        @click="profileError = null"
+        type="button"
+        class="close"
+        aria-label="Close"
+      >
+        <span aria-hidden="true">&times;</span></button
+      >{{ profileError.message }}
+    </div>
+    <!-- Profile success notification -->
+    <div
+      v-if="updateSuccessful"
+      class="alert alert-success alert-dismissable"
+      role="alert"
+    >
+      <button
+        @click="updateSuccessful = false"
+        type="button"
+        class="close"
+        aria-label="Close"
+      >
+        <span aria-hidden="true">&times;</span></button
+      >Profile updated successfully
+    </div>
+    <form>
+      <div class="form-group">
+        <label>Uuid</label>
+        <input
+          type="text"
+          class="form-control"
+          style="display: block; margin: 0 0 15px 0; width: 300px"
+          :value="$attrs.profile.uuid"
+          disabled
+        />
+        <label>Email</label>
+        <input
+          type="text"
+          class="form-control"
+          style="display: block; margin: 0 0 15px 0; width: 300px"
+          :value="$attrs.profile.email"
+          disabled
+        />
+        <label>First name</label>
+        <input
+          v-model="profile.firstName"
+          type="text"
+          class="form-control"
+          placeholder="First name"
+          style="display: block; margin: 0 0 15px 0; width: 300px"
+        />
+        <label>Last name</label>
+        <input
+          v-model="profile.lastName"
+          type="text"
+          class="form-control"
+          placeholder="Last name"
+          style="display: block; margin: 0 0 15px 0; width: 300px"
+        />
+      </div>
+      <button
+        class="btn btn-primary"
+        style="display: block"
+        @click.prevent="updateMyProfile()"
+      >
+        Update Profile
+      </button>
+    </form>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+const { updateProfile } = useIam();
+const updateSuccessful = ref(false);
+let profileError = ref(null);
+
+// Some profile values
+const profile = {
+  uuid: "",
+  firstName: "",
+  lastName: "",
+};
+
+// Get profile passed through attributes
+const attrs = useAttrs();
+profile.uuid = attrs.profile.uuid;
+profile.firstName = attrs.profile.firstName;
+profile.lastName = attrs.profile.lastName;
+
+// Attempt to update user profile
+async function updateMyProfile() {
+  console.log("profile: ", profile);
+  const { error } = await updateProfile(
+    profile.uuid,
+    profile.firstName,
+    profile.lastName
+  );
+
+  // If error, display error
+  if (error) {
+    console.log("error: ", error);
+    profileError.value = error;
+    return;
+  }
+
+  updateSuccessful.value = true;
+}
+</script>
