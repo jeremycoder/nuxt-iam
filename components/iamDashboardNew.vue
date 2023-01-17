0027ff95882fb336855b165b7698cc0521753bec
Jeremy Mwangelwa <jeremy@nizamedia.com>
2023-01-16 22:32:32 -0500

    Add name to avatar

--- components/iamDashboardNew.vue
+++ components/iamDashboardNew.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
-    <header class="navbar sticky-top flex-md-nowrap p-0 shadow">
-      <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#"
-        >Company name</a
+    <header class="navbar bg-light sticky-top flex-md-nowrap p-0 shadow">
+      <a class="navbar-brand bg-light col-md-3 col-lg-2 me-0 px-3 fs-6" href="#"
+        ><img
+          src="~~/iam/ui/img/nuxt-iam-logo-symbol.png"
+          style="width: 10%; display: inline"
+        /><span style="color: #184b81">Nuxt<b>IAM</b></span></a
       >
       <button
         class="navbar-toggler position-absolute d-md-none collapsed"
@@ -15,12 +18,7 @@
       >
         <span class="navbar-toggler-icon" @click="toggleShowMobileNav"></span>
       </button>
-      <!-- <input
-        class="form-control form-control-dark w-100 rounded-0 border-0"
-        type="text"
-        placeholder="Search"
-        aria-label="Search"
-      /> -->
+
       <div
         class="dropdown text-end"
         style="margin-right: 120px"
@@ -39,7 +37,9 @@
             height="32"
             class="rounded-circle"
           />
+          <span> Jeremy Mwangelwa</span>
         </a>
+
         <ul
           class="dropdown-menu text-small"
           :class="showProfileNav ? 'show' : ''"
@@ -51,11 +51,6 @@
           <li><a class="dropdown-item" href="#">Sign out</a></li>
         </ul>
       </div>
-      <!-- <div class="navbar-nav">
-        <div class="nav-item text-nowrap">
-          <a class="nav-link px-3" href="#">Sign out</a>
-        </div>
-      </div> -->
     </header>
 
     <div class="container-fluid">
@@ -76,123 +71,20 @@
               <li class="nav-item">
                 <a class="nav-link" href="#">
                   <span data-feather="file" class="align-text-bottom"></span>
-                  Orders
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span
-                    data-feather="shopping-cart"
-                    class="align-text-bottom"
-                  ></span>
-                  Products
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="users" class="align-text-bottom"></span>
-                  Customers
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span
-                    data-feather="bar-chart-2"
-                    class="align-text-bottom"
-                  ></span>
-                  Reports
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="layers" class="align-text-bottom"></span>
-                  Integrations
-                </a>
-              </li>
-            </ul>
-
-            <h6
-              class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase"
-            >
-              <span>Saved reports</span>
-              <a class="link-secondary" href="#" aria-label="Add a new report">
-                <span
-                  data-feather="plus-circle"
-                  class="align-text-bottom"
-                ></span>
-              </a>
-            </h6>
-            <ul class="nav flex-column mb-2">
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span
-                    data-feather="file-text"
-                    class="align-text-bottom"
-                  ></span>
-                  Current month
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span
-                    data-feather="file-text"
-                    class="align-text-bottom"
-                  ></span>
-                  Last quarter
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span
-                    data-feather="file-text"
-                    class="align-text-bottom"
-                  ></span>
-                  Social engagement
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span
-                    data-feather="file-text"
-                    class="align-text-bottom"
-                  ></span>
-                  Year-end sale
+                  Admin
                 </a>
               </li>
             </ul>
           </div>
         </nav>
 
+        <!-- Children go here -->
         <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
           <div
             class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom"
           >
             <h1 class="h2">Dashboard</h1>
-            <div class="btn-toolbar mb-2 mb-md-0">
-              <div class="btn-group me-2">
-                <button type="button" class="btn btn-sm btn-outline-secondary">
-                  Share
-                </button>
-                <button type="button" class="btn btn-sm btn-outline-secondary">
-                  Export
-                </button>
-              </div>
-              <button
-                type="button"
-                class="btn btn-sm btn-outline-secondary dropdown-toggle"
-              >
-                <span data-feather="calendar" class="align-text-bottom"></span>
-                This week
-              </button>
-            </div>
           </div>
-
-          <canvas
-            class="my-4 w-100"
-            id="myChart"
-            width="900"
-            height="380"
-          ></canvas>
 
           <h2>Section title</h2>
           <div class="table-responsive">
@@ -323,6 +215,7 @@
             </table>
           </div>
         </main>
+        <!-- End children -->
       </div>
     </div>
   </div>
