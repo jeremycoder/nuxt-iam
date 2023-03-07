282eb65d1f8afb2d3ae717cf032a32d1f7613d54
Jeremy Mwangelwa <jeremy@nizamedia.com>
2023-03-07 08:29:15 -0500

    Modify iamDashboard2. Looks great!

--- components/iamDashboard2.vue
+++ components/iamDashboard2.vue
@@ -1,40 +1,94 @@
 <template>
   <div v-if="isLoaded">
-    <header v-if="isLoggedIn && profile" class="mb-3 border-bottom">
-      <div class="container">
-        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-          <NuxtLink class="navbar-brand" to="/iam/dashboard">
-            <img src="~~/iam/ui/img/nuxt-iam-logo-symbol.png" style="width: 17%; display: inline" />
-              <span style="color: #184b81">Nuxt<b>IAM</b></span>
-            </NuxtLink>
-
-            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-              <li><a href="#" class="nav-link px-2 link-secondary">Dashboard</a></li>
-              <li><a href="#" class="nav-link px-2 link-dark">Admin</a></li>
-              <li><a href="#" class="nav-link px-2 link-dark">Profile</a></li>
-              <li><a href="#" class="nav-link px-2 link-dark">Settings</a></li>
-              <li><a href="#" class="nav-link px-2 link-dark">Products</a></li>          
-            </ul>
-          <div>
-            
-            <button class="btn btn-small btn-primary my-1" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Menu</button>
-            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
-              <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="offcanvasRightLabel">Offcanvas right</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-              </div>
-              <div class="offcanvas-body">
-                ...
-              </div>
-            </div>          
-          </div>        
-          
+    <div v-if="isLoggedIn && profile">
+      <!-- Check if email is verified -->
+      <div
+        v-if="verifyRegistrations && !emailIsVerified"
+        class="container-xl px-4 mt-4"
+      >
+        <div class="container">
+          <h2>Email verification is required</h2>
+          <div v-if="!verificationEmailSent">
+            <p>Please click the button below to verify your email</p>
+            <button
+              class="btn btn-primary"
+              type="button"
+              @click="verifyMyEmail(profile.email)"
+            >
+              Send email verification
+            </button>
+            <button
+              class="btn btn-secondary ms-2"
+              type="button"
+              @click="logMeOut()"
+            >
+              Log out
+            </button>
+          </div>
+          <div v-else>
+            <p>
+              Please check your email. Check your spam folder too. Click the
+              link in the email to verify your email. You should receive it
+              within 15 minutes.
+            </p>
+          </div>
         </div>
       </div>
-    </header>
-    <div class="container">
-      <NuxtPage />
-    </div>
+      <!-- Check if account is active -->
+      <div v-else-if="getProfileError" class="container my-5">
+        <div class="alert alert-danger" role="alert">
+          <h3 class="alert-heading">Get profile error</h3>
+          <p>
+            {{ getProfileError.message }}
+          </p>
+          <hr />
+          <button type="button" class="btn btn-secondary" @click="logMeOut">
+            Log out
+          </button>
+        </div>
+      </div>
+      <!-- Header -->
+      <div v-else>
+        <header class="mb-3 border-bottom">
+          <div class="container">
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+              <NuxtLink class="navbar-brand" to="/iam/dashboard">
+                <img src="~~/iam/ui/img/nuxt-iam-logo-symbol.png" style="width: 17%; display: inline" />
+                  <span style="color: #184b81">Nuxt<b>IAM</b></span>
+                </NuxtLink>
+
+                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                  <li><NuxtLink class="nav-link px-2 link-secondary" to="/iam/dashboard">Dashboard</NuxtLink></li>
+                  <li><a href="#" class="nav-link px-2 link-dark">Admin</a></li>
+                  <li><NuxtLink class="nav-link px-2 link-dark" to="/iam/dashboard/profile">Profile</NuxtLink></li>
+                  <li><NuxtLink class="nav-link px-2 link-dark" to="/iam/dashboard/settings">Settings</NuxtLink></li>                  
+                  <li><a href="#" class="nav-link px-2 link-dark">Products</a></li>          
+                </ul>
+              <div>
+                
+                <button class="btn btn-small btn-primary my-1" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Menu</button>
+                <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+                  <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="offcanvasRightLabel">Offcanvas right</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                  </div>
+                  <div class="offcanvas-body">
+                    ...
+                  </div>
+                </div>          
+              </div>        
+              
+            </div>
+          </div>
+      </header>
+      <!-- Main content -->
+      <main>
+        <div class="container">
+          <NuxtPage :profile="profile" @profileUpdate="getMyProfile" />
+        </div>
+      </main>      
+    </div>    
+    </div>    
   </div>
   <div v-else class="container-xl px-4 mt-4">
     <div class="spinner-border" role="status"></div>
