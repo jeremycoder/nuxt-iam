ed9817c687d4f65ded73ada3522b9c866a10aa74
Jeremy Mwangelwa <jeremy@nizamedia.com>
2023-03-12 19:08:23 -0400

    Add to error.vue and clean up server authn catchall endpoint

--- error.vue
+++ error.vue
@@ -1,10 +1,23 @@
 <template>
-  <h1>Custom Error Page</h1>
-  <button @click="handleError">Clear errors</button>
+  <div>
+    <div v-if="props.error.statusCode === 500">
+      <h1>Oh, oh, server error</h1>      
+    </div>
+    <div v-else="props.error.statusCode === 404">
+      <h1>Oh, no page does not exist!</h1>      
+    </div>
+    <div v-else>
+      <h1>We have an error!</h1>
+    </div>
+    <button @click="handleError">Clear errors</button>
+  </div>  
 </template>
+
+
 <script setup>
 const props = defineProps({
   error: Object
 })
+
 const handleError = () => clearError({ redirect: '/' })
 </script>
--- server/api/iam/authn/[...].ts
+++ server/api/iam/authn/[...].ts
@@ -1,6 +1,5 @@
 import authnController from "~~/iam/mvc/authn/controller";
 
 export default defineEventHandler(async (event) => {
-  const response = authnController(event);
-  return response;
+  return authnController(event);
 });
